bfce25a8dc3e3a53a5b9a56676cc84a4a8fb34eb
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-03-22 03:39:24 +0530

    WIP: Completed implementation of CRUD for sql in database
    Signed-off-by: Shashank Priyadarshi <shashank9163882019@gmail.com>

--- database/internal/rdbms/sql/sql.go
+++ database/internal/rdbms/sql/sql.go
@@ -3,6 +3,8 @@ package sql
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"github.com/shashank-priyadarshi/utilities"
 
 	"github.com/shashank-priyadarshi/utilities/database/models"
 	"github.com/shashank-priyadarshi/utilities/logger/ports"
@@ -24,14 +26,86 @@ func (h *Handler) Create(ctx context.Context, args ...interface{}) (*models.Resp
 	return h.Update(ctx, args...)
 }
 
+/*
+Query
+
+Add query validator to validate query and required arguments
+*/
 func (h *Handler) Query(ctx context.Context, args ...interface{}) (*models.Response, error) {
-	h.client.QueryContext(ctx, "SELECT * FROM something", args...)
-	return nil, nil
+
+	if len(args) < 2 {
+		return nil, utilities.InsufficientParameters
+	}
+
+	var (
+		err   error
+		query string
+		ok    bool
+		rows  *sql.Rows
+		model interface{}
+	)
+
+	if model = args[0]; model == nil {
+		return nil, utilities.NewError(utilities.InvalidParameter.Error(), "model")
+	}
+
+	if query, ok = args[1].(string); !ok {
+		return nil, utilities.NewError(utilities.InvalidParameter.Error(), "query")
+	}
+
+	rows, err = h.client.QueryContext(ctx, query, args[1:]...)
+	if err != nil {
+		return nil, utilities.NewError(utilities.OperationFailed.Error(), fmt.Sprintf("error executing query %s on database: %v", query, err))
+	}
+
+	var response *models.Response
+
+	for rows.Next() {
+		err = rows.Scan(model)
+		if err != nil {
+			return nil, utilities.NewError(utilities.OperationFailed.Error(), fmt.Sprintf("error scanning db row: %v", err))
+		}
+
+		response.Result = append(response.Result, model)
+	}
+
+	return response, nil
 }
 
+/*
+Update
+
+Add query validator to validate query and required arguments
+*/
 func (h *Handler) Update(ctx context.Context, args ...interface{}) (*models.Response, error) {
-	h.client.ExecContext(ctx, "SELECT * FROM TABLE ( something )", args...)
-	return nil, nil
+
+	if len(args) < 1 {
+		return nil, utilities.InsufficientParameters
+	}
+
+	var (
+		err    error
+		query  string
+		ok     bool
+		result sql.Result
+	)
+
+	if query, ok = args[1].(string); !ok {
+		return nil, utilities.NewError(utilities.InvalidParameter.Error(), "query")
+	}
+
+	result, err = h.client.ExecContext(ctx, query, args[1:]...)
+	if err != nil {
+		return nil, utilities.NewError(utilities.OperationFailed.Error(), fmt.Sprintf("error executing query %s on database: %v", query, err))
+	}
+
+	var response *models.Response
+
+	response.Result = []interface{}{
+		&result,
+	}
+
+	return response, nil
 }
 
 func (h *Handler) Delete(ctx context.Context, args ...interface{}) (*models.Response, error) {
