ad844e2a7f2b7313a52e80c483609f5a647b2e91
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-03-30 03:54:08 +0530

    fix: segregate invalid parameter and type errors
    Signed-off-by: Shashank Priyadarshi <shashank9163882019@gmail.com>

---
+++ errors.go
@@ -0,0 +1,31 @@
+package utilities
+
+import "fmt"
+
+var (
+	InsufficientParameters error = NewError("insufficient parameters")
+	InvalidParameterType   error = NewError("invalid parameter type")
+	InvalidParameter       error = NewError("invalid parameter")
+
+	OperationFailed error = NewError("operation failed")
+
+	UnsupportedType error = NewError("unsupported type")
+)
+
+type UtilityError struct {
+	errorMessage string
+}
+
+func NewError(args ...string) *UtilityError {
+	var errorMessage string
+
+	for _, arg := range args {
+		errorMessage = fmt.Sprintf("%s: %s", errorMessage, arg)
+	}
+
+	return &UtilityError{errorMessage: errorMessage}
+}
+
+func (u *UtilityError) Error() string {
+	return u.errorMessage
+}
