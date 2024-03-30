2156cc0933fd36b51d443203e76c3ad4bb4e8ebc
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-03-30 17:37:51 +0530

    refactor: remove redundant usages of type keyword in database package

--- database/models/models.go
+++ database/models/models.go
@@ -4,29 +4,31 @@ import (
 	"github.com/shashank-priyadarshi/utilities/database/constants"
 )
 
-type Config struct {
-	Options Options
-	Type    constants.Database
-}
+type (
+	Config struct {
+		Options Options
+		Type    constants.Database
+	}
 
-type Options struct {
-	LogOptions
-	DatabaseOptions
-}
+	Options struct {
+		LogOptions
+		DatabaseOptions
+	}
 
-type LogOptions struct {
-	Provider, Level, Format string
-	Trace                   bool
-}
+	LogOptions struct {
+		Provider, Level, Format string
+		Trace                   bool
+	}
 
-type DatabaseOptions struct {
-	URI, Username, Password string
-	Driver                  constants.Driver
-	ORM                     constants.ORM
-	WithORM                 bool
-	MongoDBPoolSize         uint8
-}
+	DatabaseOptions struct {
+		URI, Username, Password string
+		Driver                  constants.Driver
+		ORM                     constants.ORM
+		WithORM                 bool
+		MongoDBPoolSize         uint8
+	}
 
-type Response struct {
-	Result []interface{}
-}
+	Response struct {
+		Result []interface{}
+	}
+)
