003d35a7d48dc218ca30dbaa579d4f5fbc22db6b
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-03-30 17:33:00 +0530

    refactor: remove log DI from data package and rename constructors

--- data/internal/jwt/jwt.go
+++ data/internal/jwt/jwt.go
@@ -3,29 +3,27 @@ package jwt
 import (
 	_ "github.com/golang-jwt/jwt/v5"
 	"github.com/shashank-priyadarshi/utilities/data/ports"
-	loggerPort "github.com/shashank-priyadarshi/utilities/logger/ports"
 )
 
 // Service Provider: Create JWT with required claims
 // Parse & Verify JWT as well as JWT signer
 // Refresh JWT
 
-type JWT struct {
-	log loggerPort.Logger
+type Handler struct {
 }
 
-func NewJWTHandler(log loggerPort.Logger) (ports.Data, error) {
-	return &JWT{log: log}, nil
+func Handle() (ports.Data, error) {
+	return &Handler{}, nil
 }
 
-func (j *JWT) Generate() error {
+func (h *Handler) Generate() error {
 	return nil
 }
 
-func (j *JWT) Validate() error {
+func (h *Handler) Validate() error {
 	return nil
 }
 
-func (j *JWT) Refresh() error {
+func (h *Handler) Refresh() error {
 	return nil
 }
