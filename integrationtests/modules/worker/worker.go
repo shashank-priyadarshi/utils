0dcd273838f66d9a083951b7c0f40de2c7018dc3
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-04-11 07:16:07 +0530

    test: add integration tests for worker pool package

--- integrationtests/modules/worker/worker.go
+++ integrationtests/modules/worker/worker.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"context"
 	"fmt"
 	"github.com/shashank-priyadarshi/utilities/worker"
 	"time"
@@ -14,33 +15,65 @@ func Test() {
 	fmt.Println("Starting worker pool")
 	pool.Start()
 
-	time.Sleep(10 * time.Second)
-
 	fmt.Println("Started worker pool")
+	var (
+		id, id2 string
+	)
+
 	go func() {
-		pool.Execute(func() []interface{} {
+		pool.Execute(func(ctx context.Context) []interface{} {
 			fmt.Println("Worker running fine*1!")
 			return nil
 		})
 	}()
 	go func() {
-		pool.Execute(func() []interface{} {
-			fmt.Println("Worker running fine*2!")
-			return nil
-		})
-	}()
-	go func() {
-		pool.Execute(func() []interface{} {
-			fmt.Println("Worker running fine*3!")
-			return nil
-		})
-	}()
-	go func() {
-		pool.Execute(func() []interface{} {
-			fmt.Println("Worker running fine*4!")
-			return nil
+		id = pool.Execute(func(ctx context.Context) []interface{} {
+			for {
+				time.Sleep(30 * time.Second)
+				select {
+				case <-ctx.Done():
+					fmt.Println("Context canceled, returning function")
+					return nil
+				default:
+					fmt.Println("Worker running fine*2!")
+					return nil
+				}
+			}
 		})
 	}()
 
-	fmt.Println("Available workers:")
+	pool.DiscardJob(id)
+	go func() {
+		id2 = pool.Execute(func(ctx context.Context) []interface{} {
+			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("Context canceled, returning function")
+					return nil
+				default:
+					fmt.Println("Worker running fine*3!")
+					return nil
+				}
+			}
+		})
+	}()
+	go func() {
+		pool.Execute(func(ctx context.Context) []interface{} {
+			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("Context canceled, returning function")
+					return nil
+				default:
+					fmt.Println("Worker running fine*4!")
+					return nil
+				}
+			}
+		})
+	}()
+
+	time.Sleep(1 * time.Minute)
+	fmt.Println("Jobs IDs: ", id, id2)
+	fmt.Println("Job status: ", pool.GetJobStatus(id, "1", id2))
+	fmt.Println("Available workers:", pool.GetAvailableWorkers())
 }
