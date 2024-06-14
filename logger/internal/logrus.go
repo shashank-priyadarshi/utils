8fb7ac2820d7d5c83eddef182228b66e81e461f7
shashankpriyadarshi26946 <shashankpriyadarshi26946@gmail.com>
2024-06-14 06:32:31 +0000

    feat: add impl for logger using logrus, zap & zerolog
    Signed-off-by: Shashank Priyadarshi <shashank9163882019@gmail.com>

--- logger/internal/logrus.go
+++ logger/internal/logrus.go
@@ -1,32 +1,43 @@
 package internal
 
-type Logrus struct {
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+type LogrusLogger struct {
+	logger *logrus.Logger
 }
 
-func NewLogrus() Logrus { return Logrus{} }
-
-func (l Logrus) init() {}
-
-func (l Logrus) Info(s string, args ...interface{}) {
-	return
+func NewLogrusLogger() *LogrusLogger {
+	logger := logrus.New()
+	// Customize Logrus configuration as needed (e.g., set formatter, output)
+	return &LogrusLogger{
+		logger: logger,
+	}
 }
 
-func (l Logrus) Warn(s string, args ...interface{}) {
-	return
+func (l *LogrusLogger) Info(s string, args ...interface{}) {
+	l.logger.Infof(s, args...)
 }
 
-func (l Logrus) Error(err error, args ...interface{}) {
-	return
+func (l *LogrusLogger) Warn(s string, args ...interface{}) {
+	l.logger.Warnf(s, args...)
 }
 
-func (l Logrus) Fatal(err error, args ...interface{}) {
-	return
+func (l *LogrusLogger) Error(err error, args ...interface{}) {
+	l.logger.Errorf("%v: %s", err, fmt.Sprint(args...))
 }
 
-func (l Logrus) Debug(s string, args ...interface{}) {
-	return
+func (l *LogrusLogger) Fatal(err error, args ...interface{}) {
+	l.logger.Fatalf("%v: %s", err, fmt.Sprint(args...))
 }
 
-func (l Logrus) With(args ...interface{}) {
-	return
+func (l *LogrusLogger) Debug(s string, args ...interface{}) {
+	l.logger.Debugf(s, args...)
+}
+
+func (l *LogrusLogger) With(args ...interface{}) {
+	// TODO: Implement structured logging with fields using logrus.Fields
 }
