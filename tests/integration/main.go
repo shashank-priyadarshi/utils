ae373a0a2f86fdf1248db302fef74f578397de9e
Shashank Priyadarshi <shashank9163882019@gmail.com>
2024-08-15 23:30:02 +0530

    chore: upgrade go version and deps to latest
    Signed-off-by: Shashank Priyadarshi <shashank9163882019@gmail.com>

--- tests/integration/main.go
+++ tests/integration/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/ory/dockertest/v3"
-	"github.com/shashank-priyadarshi/utilities/integrationtests/suite"
+	"github.com/shashank-priyadarshi/utilities/tests/integration/suite"
 )
 
 func main() {
