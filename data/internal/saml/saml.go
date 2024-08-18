d618769f79cda850738be8c5d2eb1c5b84ce23d1
Shashank Priyadarshi <shashank9163882019@gmail.com>
2024-08-19 02:37:58 +0530

    refactor(secretxform): remove redundant impl for JWT & SAML from data pkg
    Signed-off-by: Shashank Priyadarshi <shashank9163882019@gmail.com>

--- data/internal/saml/saml.go
+++
@@ -1,7 +0,0 @@
-package saml
-
-// Service Provider: Create SAML request
-// Identity Service Provider: Parse & Verify SAML, Return signed SAML Assertion
-// Service Provider: Parse & Verify SAML signature and ACL
-
-// Refresh token
--- data/ports/ports.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-type Data interface {
-	Generate(map[string]interface{}) (string, error)
-	Validate(string, map[string]interface{}) error
-	Refresh(string, map[string]interface{}) (string, error)
-}
